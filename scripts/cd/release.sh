3638beb42c5a3a1b32a2284f6ec47085ed310bc7
Daniel Sawano <daniel@sawano.se>
2018-02-12 09:50:34 +0100

    Updating release script

--- scripts/cd/release.sh
+++ scripts/cd/release.sh
@@ -6,6 +6,6 @@ JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 export JAVA_HOME="${JAVA_HOME}"
 
 echo "Releasing new version..."
-./mvnw release:clean
-./mvnw release:prepare
-./mvnw release:perform
+./mvnw -Prelease release:clean
+./mvnw -Prelease release:prepare
+./mvnw -Prelease release:perform
